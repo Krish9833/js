52cd1695ac72c76fe09fd46d9a1021e37c605dfd
Krish9833 <129373586+Krish9833@users.noreply.github.com>
2024-07-23 18:34:05 +0000

    Operations

---
+++ javascript/04_Operation.js
@@ -0,0 +1,42 @@
+ let value = 3
+ let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "namaste"
+let str2 = "guruji"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//console.log((3 + 4) * 5 % 3);  Don't write messy code like this!!!.......
+ 
+// console.log(true);
+
+//console.log(+true); not preferable to write  code like this
+//console.log(+""); not preferable to write code like this
+
+let num1, num2, num3 
+
+//num1 = num2 = num3 = 2+2 //Don't write like this
+// In coding visibility matters and code should be neat and clean
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+// prefix ke andar value pehle hi implement ho jati hai
+// postfix me value baad me implement hoti hai
+
+
+// GO TO MDN FOR INCREMENT value( postfix & prefix)
